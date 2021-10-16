010a6ca9225918d0b8d59fe534fd4409b1d9216f
MrLogEN <mrlogen1299@gmail.com>
2021-10-16 15:47:59 +0200

    Script for movements

---
+++ Maturita 2D game/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+    }
+}
