9020a79ee92d74fb34e46926b159f08ce5dfcfa5
MrLogEN <mrlogen1299@gmail.com>
2021-11-28 18:18:32 +0100

    Checking distance between the boss and the blayer

--- Maturita 2D game/Assets/Script/Boss/Boss_3/Boss3Script.cs
+++ Maturita 2D game/Assets/Script/Boss/Boss_3/Boss3Script.cs
@@ -24,6 +24,7 @@ public class Boss3Script : MonoBehaviour, IBoss
 
     public bool isInvincible;
 
+    public GameObject player;
     public void TakeDamage()
     {
         
@@ -55,10 +56,28 @@ public class Boss3Script : MonoBehaviour, IBoss
         if (CurrentPhase == 1)
         {
             //actions for phase 1
+
+            //checking if the boss is not in animation is needed.
+            Vector3 distance = DistanceCheck(player); //checking how far is the player from the boss
+            if (distance.x <= 3f && distance.y<=3f) // if the player is far 3f or less - execute following code
+            {
+                Debug.Log("Slash");
+                //Slash animation
+            }
+            
         }
         if (CurrentPhase == 2)
         {
             //actions for phase 2
+            //random generator for deciding - straight arrow fire or 3 arrows fired at once
         }
+    }
+    Vector3 DistanceCheck(GameObject player)
+    {
+        float x = Mathf.Abs(player.transform.position.x - gameObject.transform.position.x); //absolute x value of substracted positions of the boss and of the player
+        float y = Mathf.Abs(player.transform.position.y - gameObject.transform.position.y); //absolute y value of substracted positions of the boss and of the player
+        float z = Mathf.Abs(player.transform.position.z - gameObject.transform.position.z); //absolute z value of substracted positions of the boss and of the player
+        Vector3 sub = new Vector3(x, y, z); //Creating Vector3 from the differences
+        return sub;
     }
 }
