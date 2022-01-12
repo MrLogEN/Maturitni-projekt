29c1484bbc2f124e423edfb231ed879f8f8af61d
Vilém Charwot <68494540+MrLogEN@users.noreply.github.com>
2022-01-12 16:15:12 +0100

    Revert "Save load progress"

---
+++ Maturita 2D game/Assets/Script/Other/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ICollisonHandler
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+    public void CollisionEnter(string colliderTag, GameObject other)
+    {
+        if (colliderTag == "BossAttack" && other.tag == "Player")
+        {
+            other.GetComponent<PlayerActions>().TakeHit();
+        }
+    }
+}
