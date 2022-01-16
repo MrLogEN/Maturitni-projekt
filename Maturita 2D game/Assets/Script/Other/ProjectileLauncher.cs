b076ab27a2b299e5f318f034524a6d6756fe3265
MrLogEN <mrlogen1299@gmail.com>
2022-01-16 22:06:27 +0100

    Squashed commit of the following:
    commit 7cc003b546d86e9e8e6ac373a68dced01bc7ceab
    Author: Toshia000 <83763475+Toshia000@users.noreply.github.com>
    Date:   Mon Dec 6 16:24:52 2021 +0100
    
        Player take hit
    
    commit 563ecb488bccf76ca2a29d373f7d1e7e3ed3f88a
    Author: Toshia000 <83763475+Toshia000@users.noreply.github.com>
    Date:   Sun Dec 5 18:58:30 2021 +0100
    
        Detect Collisions
    
    commit 1c74ca71409a57c10095f14169cca99f0ab9860e
    Author: Toshia000 <83763475+Toshia000@users.noreply.github.com>
    Date:   Sat Nov 27 19:57:46 2021 +0100
    
        Cannon
    
    commit c10c379dd2e46e141fa5def9e73c43aac416368e
    Author: Toshia000 <83763475+Toshia000@users.noreply.github.com>
    Date:   Thu Nov 18 20:29:25 2021 +0100
    
        level_1_v1

---
+++ Maturita 2D game/Assets/Script/Other/ProjectileLauncher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    public GameObject projectileGameobject;  // prefab(střela), který se klonuje do scény
+    private GameObject projectileClone; // instance objektu 
+    private Rigidbody2D projectile; // Rigidbody2D komponent objektu
+    public Transform target; // cíl, na který se střílí
+    public float height = 5;  // maximální výška trajektorie střely
+    public float gravity = -18; // hodnota gravitace
+    public float timeToLive = 3; // čas, po kterém se odstraní instance objektu ze scény
+    public float fireRate = 2; // čas, za který se jednou vystřelí
+    private float nextFire = 0; // čas, ve který proběhne další výstřel
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Launch();
+        }
+        Destroy(projectileClone, timeToLive); // zničení objektu ze scény za určitý čas
+    }
+
+    void Launch()
+    {
+        projectileClone = Instantiate(projectileGameobject, transform.position, transform.rotation); // vytvoření instance objektu ve scéně
+        projectile = projectileClone.GetComponent<Rigidbody2D>(); // získání komponenty Rigidbody2D z vytvořené instance
+        Physics2D.gravity = Vector2.up * gravity; // přiřazení gravitace
+        projectile.velocity = CalculateLaunchVelocity(); // přiřazení vypočítané rychlosti komponentě Rigidbody2D
+        print(CalculateLaunchVelocity());
+    }
+
+    // metoda na výpočet trajektorie pohybu
+    Vector2 CalculateLaunchVelocity()
+    {
+        float displacementY = target.position.y - projectile.position.y;
+        Vector2 displacementX = new Vector2(target.position.x - projectile.position.x, 0);
+        Vector2 velocityY = Vector2.up * Mathf.Sqrt(-2 * gravity * height);
+        Vector2 velocityX = displacementX / (Mathf.Sqrt(-2 * height / gravity) + Mathf.Sqrt(2 * (displacementY - height) / gravity));
+        return velocityX + velocityY;
+    }
+}
