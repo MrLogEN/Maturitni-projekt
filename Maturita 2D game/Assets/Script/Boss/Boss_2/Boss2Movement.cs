d0573f8456a4350a5dc8233bf1eedb7270834082
Filip-Kovac <79059598+Filip-Kovac@users.noreply.github.com>
2022-01-16 18:28:50 +0100

    DONE
    Done vy hovna

--- Maturita 2D game/Assets/Script/Boss/Boss_2/Boss2Movement.cs
+++ Maturita 2D game/Assets/Script/Boss/Boss_2/Boss2Movement.cs
@@ -5,20 +5,15 @@ using System.Threading;
 
 public class Boss2Movement : MonoBehaviour
 {
-    public float degreesPerSecond = 15.0f;
-    public float amplitude = 0.5f;
-    public float frequency = 1f;
     public bool spell1;
     public bool spell2;
     public float m;
     public Animator anim;
-
-
-    Vector3 posOffset = new Vector3();
-    Vector3 tempPos = new Vector3();
+    public GameObject spikePrefab;
+    public GameObject rootPrefab;
+    List<GameObject> spikes = new List<GameObject>();
     void Start()
     {
-        posOffset = transform.position;
         spell1 = false;
         spell2 = false;
         anim = GetComponent<Animator>();
@@ -27,7 +22,7 @@ public class Boss2Movement : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        if (Time.fixedTime % 5 == 0)
+        if (Time.fixedTime % 8 == 0)
         {
             System.Random rn = new System.Random();
             int random = rn.Next(0, 2);
@@ -42,47 +37,25 @@ public class Boss2Movement : MonoBehaviour
         }
         if (spell2 == true)
         {
-            if (tempPos.y !=-2.5f)
+            if (transform.position.y == -2.5f)
             {
-                tempPos = posOffset;
-                tempPos.y += 5f * Mathf.Sin((Time.fixedTime - m)* Mathf.PI * frequency * 0.5f) * amplitude;
-                transform.position = tempPos;
-            }
-            else
-            {
-                //zapne animiaci
-                //anim.SetBool("Attack2Start", true);
                 anim.SetBool("Attack2Start", true);
-                if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack2-End"))//èeká než dokonèí animaci
+                if (spikes.Count == 0)
                 {
-                    anim.SetBool("Attack2Start", false);
-                    SpellTwoEnd();
-                    m = m + 2; 
+                    GameObject a = Instantiate(spikePrefab) as GameObject;
+                    a.transform.position = new Vector2(GameObject.Find("Player").transform.position.x, -4.5f);
+                    spikes.Add(a);
                 }
-                
             }
 
         }
         else if (spell1 == true)
         {
             float player_postion = GameObject.Find("Player").transform.position.y;
-            if (player_postion == tempPos.y)
+            if (player_postion - transform.position.y <= 0.1f)
             {
-
+                anim.SetBool("Attack1Start", true);
             }
-            else
-            {
-                Debug.Log("nig");
-                //zapne animaci
-                SpellOneEnd();
-            }
-
-        }
-        else
-        {
-            tempPos = posOffset;
-            tempPos.y += 5f * Mathf.Sin((Time.fixedTime - m)* Mathf.PI * frequency * 0.5f) * amplitude;
-            transform.position = tempPos;
         }
     }
     public void SpellOne()
@@ -96,9 +69,13 @@ public class Boss2Movement : MonoBehaviour
     public void SpellOneEnd()
     {
         spell1 = false;
+        anim.SetBool("Attack1Start", false);
     }
     public void SpellTwoEnd()
     {
         spell2 = false;
+        anim.SetBool("Attack2Start", false);
+        Destroy(spikes[0]);
+        spikes.Clear();
     }
 }
