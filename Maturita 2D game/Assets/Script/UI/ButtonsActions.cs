57a10f30781f5a1e2eea4924b2f6ab9d7b2fe4c5
MrLogEN <mrlogen1299@gmail.com>
2022-01-17 13:09:37 +0100

    fdsaf

--- Maturita 2D game/Assets/Script/UI/ButtonsActions.cs
+++ Maturita 2D game/Assets/Script/UI/ButtonsActions.cs
@@ -75,53 +75,108 @@ public class ButtonsActions : MonoBehaviour
         }
         else if (isClickedRight)
         {
-
+            WaitongForKey(ref right, rightButton, ref isClickedRight);
+            rightButton.GetComponentInChildren<Text>().text = right.ToString();
         }
         else if (isClickedLeft)
         {
-
+            WaitongForKey(ref left, leftButton, ref isClickedLeft);
+            leftButton.GetComponentInChildren<Text>().text = left.ToString();
         }
         else if (isClickedCrouch)
         {
-
+            WaitongForKey(ref crouch, crouchButton, ref isClickedCrouch);
+            crouchButton.GetComponentInChildren<Text>().text = crouch.ToString();
         }
         else if (isClickedUp)
         {
-
+            WaitongForKey(ref up, upButton, ref isClickedUp);
+            upButton.GetComponentInChildren<Text>().text = up.ToString();
         }
         else if (isClickedShoot)
         {
-
+            WaitongForKey(ref shoot, shootButton, ref isClickedShoot);
+            shootButton.GetComponentInChildren<Text>().text = shoot.ToString();
         }
         else if (isClickedSpecialAbility)
         {
-
+            WaitongForKey(ref specialAbility, specialAbilityButton, ref isClickedSpecialAbility);
+            specialAbilityButton.GetComponentInChildren<Text>().text = specialAbility.ToString();
         }
         else if (isClickedSelectUp)
         {
-
+            WaitongForKey(ref selectUp, selectUpButton, ref isClickedSelectUp);
+            selectUpButton.GetComponentInChildren<Text>().text = selectUp.ToString();
         }
         else if (isClickedSelectDown)
         {
-
+            WaitongForKey(ref selectDown, selectDownButton, ref isClickedSelectDown);
+            selectDownButton.GetComponentInChildren<Text>().text = selectDown.ToString();
         }
         else if (isClickedSelectLeft)
         {
-
+            WaitongForKey(ref selectLeft, selectLeftButton, ref isClickedSelectLeft);
+            selectLeftButton.GetComponentInChildren<Text>().text = selectLeft.ToString();
         }
         else if (isClickedSelectRight)
         {
-
+            WaitongForKey(ref selectRight, selectRightButton, ref isClickedSelectRight);
+            selectRightButton.GetComponentInChildren<Text>().text = selectRight.ToString();
         }
         else if (isClickedSelectSelect)
         {
-
+            WaitongForKey(ref selectSelect, selectSelectButton, ref isClickedSelectSelect);
+            selectSelectButton.GetComponentInChildren<Text>().text = selectSelect.ToString();
         }
 
     }
     public void OnJumpButtonClick()
     {
         isClickedJump = true;
+    }
+    public void OnLookUpClick()
+    {
+        isClickedUp = true;
+    }
+    public void OnLeftClick()
+    {
+        isClickedLeft = true;
+    }
+    public void OnRightClick()
+    {
+        isClickedRight = true;
+    }
+    public void OnCrouchClick()
+    {
+        isClickedCrouch = true;
+    }
+    public void OnShootClick()
+    {
+        isClickedShoot = true;
+    }
+    public void OnSpecialAbilityClick()
+    {
+        isClickedSpecialAbility = true;
+    }
+    public void OnSelectUpClick()
+    {
+        isClickedSelectUp = true;
+    }
+    public void OnSelectDownClick()
+    {
+        isClickedSelectDown = true;
+    }
+    public void OnSelectLeftClick()
+    {
+        isClickedSelectLeft = true;
+    }
+    public void OnSelectRightClick()
+    {
+        isClickedSelectRight = true;
+    }
+    public void OnSelectSelectClick()
+    {
+        isClickedSelectSelect = true;
     }
     private void WaitongForKey(ref KeyCode key, Button but, ref bool isClicked)
     {
