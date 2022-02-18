3c945ae9feb11d73013fb71005996bfd6b5ed97a
MrLogEN <mrlogen1299@gmail.com>
2022-02-18 16:45:34 +0100

    transition

--- Maturita 2D game/Assets/Script/UI/LoadingManager.cs
+++ Maturita 2D game/Assets/Script/UI/LoadingManager.cs
@@ -9,7 +9,8 @@ public class LoadingManager : MonoBehaviour
     [SerializeField] private GameObject _loaderCanvas;
     [SerializeField] private Slider _progressBar;
     private float _target;
-
+    [SerializeField] private GameObject _transitionCanvas;
+    [SerializeField] private Animator _animator;
     void Awake()
     {
         if (instance ==null)
@@ -26,18 +27,26 @@ public class LoadingManager : MonoBehaviour
     {
         _target = 0;
         _progressBar.value = 0;
+        _transitionCanvas.SetActive(true);
         var scene = SceneManager.LoadSceneAsync(sceneName);
-
         scene.allowSceneActivation = false;
 
         _loaderCanvas.SetActive(true);
+        
         do
         {
             //await Task.Delay(100);
             _target = scene.progress;
         } while (scene.progress < 0.9f);
+        await Task.Delay(200);
         scene.allowSceneActivation = true;
+
+        _animator.SetTrigger("Start");
+        await Task.Delay(1000);
         _loaderCanvas.SetActive(false);
+        _animator.SetTrigger("End");
+        await Task.Delay(1000);
+        _transitionCanvas.SetActive(false);
     }
     void Update()
     {
